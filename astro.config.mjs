cf158a0884866c2684b7cf55178103669437f891
Shiddhartha Pal <pal35-1069@diu.edu.bd>
2026-02-28 21:02:16 +0600

    set the astro.config.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   integrations: [react()],
-  output:'server',
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  output: "server",
+  adapter: node({ mode: "standalone" }),
+  vite: { plugins: [tailwindcss()] },
 });
